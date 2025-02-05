eead3081a8eae23fcb105036a11612db442ade5f
vimal-2004 <148618715+vimal-2004@users.noreply.github.com>
2025-02-05 09:55:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u vimal2004 -p 1234567890
+    docker tag test vimal2004/task2
+    docker push vimal2004/task2
     
